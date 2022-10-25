638c5ead4028addbb547b988aa0276c80c8642b2
kpw6 <kpw6@njit.edu>
2022-10-25 15:42:57 -0400

    Organization + other stuff

---
+++ Database/mysqlconnect.php
@@ -0,0 +1,32 @@
+#!/usr/bin/php
+<?php
+require_once('../path.inc');
+require_once('../get_host_info.inc');
+require_once('../rabbitMQLib.inc');
+require_once('Connection.php');
+require_once('login.php.inc');
+
+
+function requestProcessor($request)
+{
+  echo "received request".PHP_EOL;
+  var_dump($request);
+  if(!isset($request['type']))
+  {
+    return "ERROR: unsupported message type";
+  }
+  switch ($request['type'])
+  {
+    case "login":
+      return validateLogin($request['username'],$request['password']);
+    case "validate_session":
+      return doValidate($request['sessionId']);
+  }
+  return array("returnCode" => '0', 'message'=>"Server received request and processed");
+}
+
+
+$server = new rabbitMQServer("testRabbitMQ.ini","testServer");
+echo "server started";
+$server->process_requests('requestProcessor');
+?>
