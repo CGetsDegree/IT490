a95767e49a0202c121aa730f23018262e369b37c
Fontbane <fontbane@gmail.com>
2022-11-01 19:28:46 -0400

    More watchlist

--- public_html/movies.php
+++ public_html/movies.php
@@ -1,16 +1,9 @@
 <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');
 require_once('rabbitFunctions.php');
 if ($_POST){
       $rtype=$_POST["type"];
       $sessionid = $_POST["sessionid"];
       $searchquery=$_POST["searchquery"];
-      $movies=array();
-      if ($_POST["movies"]){
-	      $movies=json_decode($_POST["movies"]);
-      }
       $request = array();
       $request['type'] = $rtype;
       $request['sessionid'] = $sessionid;
@@ -19,6 +12,8 @@ if ($_POST){
 	$request['page']=$page;
       }
       else if ($rtype=="addToWatchlist"){
+	      $movies=array();
+	      $movies=json_decode($_POST["movies"])
 	      $request["movies"]=$movies;
       }
       $response = sendAPI($request);
