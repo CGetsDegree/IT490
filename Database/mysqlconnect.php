6a40e798747cbf5194df6e0e1bf4744721e6feec
kpw6 <kpw6@njit.edu>
2022-11-01 19:11:38 -0400

    Functions added for the forum

--- Database/mysqlconnect.php
+++ Database/mysqlconnect.php
@@ -5,6 +5,7 @@ require_once('../get_host_info.inc');
 require_once('../rabbitMQLib.inc');
 require_once('Connection.php');
 require_once('rFunctions.inc');
+require_once('forumFunctions.inc');
 
 
 function requestProcessor($request)
@@ -20,27 +21,35 @@ function requestProcessor($request)
     case "login":
       return validateLogin($request['username'],$request['password']);
     case "validate_session":
-      return validateSeassion($request['sessionId']);
+      return validateSession($request['sessionid']);
     case "logout":
-      return stopSeassion($request['sessionId']);
+      return stopSession($request['sessionid']);
     case "register":
     	return validateRegister($request['username'],$request['password']);
     case "add_service_list":
-    	return addServices($request['username'], $request['serviceID']);
+    	return addServices($request['username'], $request['serviceid']);
     case "remove_service_list":
-    	return removeService($request['username'], $request['serviceID']);
+    	return removeService($request['username'], $request['serviceid']);
     case "get_service_list":
     	return getServices($request['username']);
     case "add_movie_list":
-    	return addServices($request['username'], $request['movieID']);
+    	return addServices($request['username'], $request['movieid']);
     case "remove_movie_list":
-    	return removeServices($request['username'], $request['movieID']);
+    	return removeServices($request['username'], $request['movieid']);
     case "get_movie_list":
     	return getMovies($request['username']);
     case "change_movie_rating":
-    	return changeRating($request['username'], $request['movieID'], $request['rating']);
+    	return changeRating($request['username'], $request['movieid'], $request['rating']);
     case "get_movie_rating":
-    	return getRating($request['username'], $request['movieID']);
+    	return getRating($request['username'], $request['movieid']);
+    case "get_forum_posts":
+    	return sendForumPosts($request['forumTopic']);
+    case "get_forum_topics":
+    	return sendForumTopics();
+    case "create_forum_topic":
+    	return createForumTopic(request['username'], request['forumName'], request['postText']);
+    case "create_forum_post":
+    	return createForumPost(request['username'], request['postText'], request['forumTopic']);
     
   }
   return json_encode(array("returnCode" => '0', 'message'=>"Server received request and processed"));
@@ -48,6 +57,6 @@ function requestProcessor($request)
 
 
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
-echo "server started";
+echo "server started".PHP_EOL;
 $server->process_requests('requestProcessor');
 ?>
