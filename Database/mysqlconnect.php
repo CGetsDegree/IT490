1326cb2a44db3349e88116cde718280b087273fa
kpw6 <kpw6@njit.edu>
2022-11-01 13:57:05 -0400

    cool

--- Database/mysqlconnect.php
+++ Database/mysqlconnect.php
@@ -5,7 +5,7 @@ require_once('../get_host_info.inc');
 require_once('../rabbitMQLib.inc');
 require_once('Connection.php');
 require_once('rFunctions.inc');
-include('../sendLog.php');
+include_once('../sendLog.php');
 
 echo SendToLogger("DB Server Startup");
 
@@ -22,27 +22,27 @@ function requestProcessor($request)
     case "login":
       return validateLogin($request['username'],$request['password']);
     case "validate_session":
-      return validateSession($request['sessionId']);
+      return validateSession($request['sessionid']);
     case "logout":
-      return stopSession($request['sessionId']);
+      return stopSession($request['sessionid']);
     case "register":
     	return validateRegister($request['username'],$request['password']);
     case "add_service_list":
-    	return addServices($request['username'], $request['serviceID']);
+    	return addServices($request['username'], $request['serviceid']);
     case "remove_service_list":
-    	return removeService($request['username'], $request['serviceID']);
+    	return removeService($request['username'], $request['serviceid']);
     case "get_service_list":
     	return getServices($request['username']);
     case "add_movie_list":
-    	return addServices($request['username'], $request['movieID']);
+    	return addServices($request['username'], $request['movieid']);
     case "remove_movie_list":
-    	return removeServices($request['username'], $request['movieID']);
+    	return removeServices($request['username'], $request['movieid']);
     case "get_movie_list":
     	return getMovies($request['username']);
     case "change_movie_rating":
-    	return changeRating($request['username'], $request['movieID'], $request['rating']);
+    	return changeRating($request['username'], $request['movieid'], $request['rating']);
     case "get_movie_rating":
-    	return getRating($request['username'], $request['movieID']);
+    	return getRating($request['username'], $request['movieid']);
     
   }
   return json_encode(array("returnCode" => '0', 'message'=>"Server received request and processed"));
